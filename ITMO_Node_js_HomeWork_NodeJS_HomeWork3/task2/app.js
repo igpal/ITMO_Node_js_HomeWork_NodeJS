7b4e08f4dbd75f05db45891a29ea67f70b6524e9
Howl21 <igor_palchunov@mail.com>
2020-02-05 19:16:15 +0300

    hw3

---
+++ ITMO_Node_js_HomeWork_NodeJS_HomeWork3/task2/app.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+
+http.createServer(function(req, res) {
+    let urlObj = url.parse(req.url);
+    let obj = {};
+
+    if (urlObj.query) {
+        obj = querystring.parse(urlObj.query);
+    }
+
+    if (obj.getUsers) {
+        fs.readFile('users.json', 'utf-8', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                return res.end();
+            }
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end(data);
+        });
+    } else {
+        fs.readFile('index.html', 'utf-8', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                return res.end();
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        });
+    }
+
+}).listen(8080);
